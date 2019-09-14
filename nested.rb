4ae9688c6aac61d96af9204bb330c1fe56e47e1f
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-14 09:12:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,7 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     newdennislanguage = ["C", "Assembley"]
+     newdennislanguage = ["C", "Assembly"]
      programmer_hash[:dennis_ritchie][:languages]=newdennislanguage
      programmer_hash
 
