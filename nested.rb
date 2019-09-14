02c04dca5108b13c2d1547087635bbfaf7408345
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-14 09:07:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,8 @@ def adding_matz
      }
 programmer_hash[:yukihiro_matsumoto]={}
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages]=
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+programmer_hash
     
 end
 
